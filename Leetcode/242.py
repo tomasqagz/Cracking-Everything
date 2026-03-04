69187d607f76fb0baf8cff3e5d1b6ee6309ab4f4
Tomas Qagz <tomasqagz@gmail.com>
2026-03-04 20:05:18 -0300

    O(n) solution

--- Leetcode/242.py
+++ Leetcode/242.py
@@ -1,4 +1,7 @@
+from typing import Counter
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        return Counter(s) == Counter(t)
 # Given two strings s and t, return true if t is an anagram of s, and false otherwise.
